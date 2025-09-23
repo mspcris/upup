c3a5d5d8777ddffdc96b756c1027876d2c78ccef
Cristiano Silva Souza <msp.cris@gmail.com>
2025-09-23 04:25:53 -0300

    Ajustes de header Logo na home Rodapé padronizado JS apontado no header

---
+++ .js/carossel.js
@@ -0,0 +1,11 @@
+  
+        (function () {
+            const radios = Array.from(document.querySelectorAll('.carousel input[name="radio-btn"]'));
+            if (!radios.length) return;
+            let i = radios.findIndex(r => r.checked);
+            setInterval(() => {
+                i = (i + 1) % radios.length;
+                radios[i].checked = true; // isto dispara suas regras CSS (#radioX:checked ~ .slides ...)
+            }, 4000); // 4s por slide
+        })();
+ 
