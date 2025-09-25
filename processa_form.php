b381155e5988f17e4c9d3bb463eb2ee11af47bac
Cristiano Silva Souza <msp.cris@gmail.com>
2025-09-25 02:14:19 -0300

    form php

--- processa_form.php
+++ processa_form.php
@@ -1,35 +1,24 @@
 <?php
-// Configurações do e-mail
-$destinatario = "upupoficial@gmail.com";  // coloque o e-mail da ONG
-$assunto = "Novo pedido de inscrição no curso - UPUP";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome   = $_POST['nome'] ?? '';
+    $data   = $_POST['data_nascimento'] ?? '';
+    $end    = $_POST['endereco'] ?? '';
+    $fone   = $_POST['telefone'] ?? '';
 
-// Captura os dados do formulário
-$nome       = $_POST['nome'] ?? '';
-$nascimento = $_POST['nascimento'] ?? '';
-$endereco   = $_POST['endereco'] ?? '';
-$telefone   = $_POST['telefone'] ?? '';
+    $to      = "contato@upup.ong.br"; // coloque aqui um e-mail criado no painel da KingHost
+    $subject = "Nova inscrição pelo site UPUP";
+    $message = "Nome: $nome\n".
+               "Data de Nascimento: $data\n".
+               "Endereço: $end\n".
+               "Telefone: $fone\n";
 
-// Monta a mensagem
-$mensagem = "
-<strong>Nova solicitação de inscrição recebida:</strong><br><br>
-<b>Nome:</b> $nome <br>
-<b>Data de Nascimento:</b> $nascimento <br>
-<b>Endereço:</b> $endereco <br>
-<b>Telefone:</b> $telefone <br>
-";
+    $headers = "From: contato@upup.ong.br\r\n".
+               "Reply-To: $to\r\n";
 
-// Cabeçalhos (importante para evitar SPAM)
-$headers  = "MIME-Version: 1.1\r\n";
-$headers .= "Content-type: text/html; charset=UTF-8\r\n";
-$headers .= "From: contato@upup.ong.br\r\n";  // use um e-mail do seu domínio
-$headers .= "Reply-To: $destinatario\r\n";
-
-// Envia o e-mail
-if (mail($destinatario, $assunto, $mensagem, $headers)) {
-    echo "<h2>✅ Inscrição enviada com sucesso!</h2>";
-    echo "<a href='cursos_artesanato.html'>Voltar</a>";
-} else {
-    echo "<h2>❌ Ocorreu um erro ao enviar sua inscrição.</h2>";
-    echo "<a href='cursos_artesanato.html'>Tente novamente</a>";
+    if (mail($to, $subject, $message, $headers)) {
+        echo "✅ Formulário enviado com sucesso!";
+    } else {
+        echo "❌ Erro ao enviar. Verifique o PHP Mail da hospedagem.";
+    }
 }
 ?>
